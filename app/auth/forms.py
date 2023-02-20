7dd6f218304b02a2cb0f9be4a404fd8d9d819db7
Cagey7 <dagodecagey@gmail.com>
2023-02-20 18:04:07 +0600

    Added basic design with bootstrap

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     """Flask wtf registration class"""
     email = StringField("Электронная почта", validators=[DataRequired("Введите электронную почту"), 
                                                          Email("Неправильный формат электронной почты"),
-                                                         Length(max=120)])
+                                                         Length(max=120)], render_kw={"autofocus": True})
     password = PasswordField("Пароль", validators=[DataRequired("Введите пароль"), 
                                                    EqualTo("confirm_password", message="Пароли не совпадают"),
                                                    Length(min=8, max=20, message="Пароль должен содержать от 8 до 20 символов")])
@@ -24,7 +24,7 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     """Flask wtf login class"""
-    email = StringField("Электронная почта", validators=[DataRequired("Введите электронную почту")])
+    email = StringField("Электронная почта", validators=[DataRequired("Введите электронную почту")], render_kw={"autofocus": True})
     password = PasswordField("Пароль", validators=[DataRequired("Введите пароль")])
     remember_me = BooleanField("Запомнить меня")
     submit = SubmitField("Войти")
