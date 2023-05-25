e3f003c31935aed1d25cb173e35b574d9483c8ec
Cagey7 <dagodecagey@gmail.com>
2023-05-25 23:54:19 +0600

    Added functions to models to reset password

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from app import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
@@ -47,6 +49,26 @@ class User(db.Model, UserMixin):
     def verify_password(self, password):
         """Compare inputted password with password in database"""
         return check_password_hash(self.password_hash, password)
+
+
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config["SECRET_KEY"], expiration)
+        return s.dumps({"reset": self.id})
+
+
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        user = User.query.get(data.get("reset"))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.commit()
+        return True
 
 
 class WaitingMessage(db.Model):
